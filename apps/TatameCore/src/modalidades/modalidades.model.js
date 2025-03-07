193935a4cac398268b1fa05305d35c4bba796d3b
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-03-07 12:28:20 +0100

    ✨ feat: actualizar modalidades

--- apps/TatameCore/src/modalidades/modalidades.model.js
+++ apps/TatameCore/src/modalidades/modalidades.model.js
@@ -1,4 +1,4 @@
-import { connection } from "../database/config";
+import { connection } from "../database/config.js";
 
 async function create(nomeModalidade) {
   const query = `
@@ -39,9 +39,24 @@ async function getById(modalidadeId) {
     }
 }
 
+async function update(modalidadeId) {
+    const query = `
+      UPDATE modalidades SET nome_modalidade = ?  
+      WHERE modalidade_id = ?;
+      `;
+    try {
+      const [result] = await connection.query(query, [modalidadeId]);
+      return result;
+    } catch (error) {
+      console.error('Erro ao buscar modalidade: ', error);
+    }
+}
+
 
 export default {
   create,
   getAll,
-  getById
+  getById,
+  update,
+  deleteModalidade
 };
