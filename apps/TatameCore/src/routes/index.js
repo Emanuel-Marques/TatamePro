5f9416ffac7cad49cba33381e8ed8471d2872ee3
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-03-07 19:41:37 +0100

    :sparkles: feat: rotas do endpoint dos planos

--- apps/TatameCore/src/routes/index.js
+++ apps/TatameCore/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import modalidadesRoutes from '../modalidades/modalidades.routes.js';
 import utilizadoresRoutes from '../utilizadores/utilizadores.routes.js';
+import planosRoutes from '../planos/planos.routes.js';
 const router = express.Router();
 
 router.use('/modalidades', modalidadesRoutes);
 router.use('/utilizadores', utilizadoresRoutes);
+router.use('/planos', planosRoutes);
 
 
 export default router;
