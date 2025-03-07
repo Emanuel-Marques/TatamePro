27962d6c218eb0d6344c1de4cea427beecf6a79f
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-03-07 10:10:21 +0100

    ✨ feat: buscar todas modalidades

--- apps/TatameCore/src/modalidades/modalidades.model.js
+++ apps/TatameCore/src/modalidades/modalidades.model.js
@@ -2,7 +2,7 @@ import { connection } from "../database/config";
 
 async function create(nomeModalidade) {
   const query = `
-    INSERT INTO alunos (nome_modalidade) 
+    INSERT INTO modalidades (nome_modalidade) 
     VALUES (?);
     `;
   try {
@@ -13,6 +13,21 @@ async function create(nomeModalidade) {
   }
 }
 
+async function getAll() {
+    const query = `
+      SELECT *  FROM modalidades;
+      `;
+    try {
+      const [result] = await connection.query(query, [nomeModalidade]);
+      return result;
+    } catch (error) {
+      console.error('Erro ao buscar modalidade: ', error);
+    }
+}
+
+
 export default {
   create,
+  getAll,
+
 };
