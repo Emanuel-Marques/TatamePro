03eda0db7c38ff32a5676e5ee0797be23a8556b7
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-03-07 18:37:06 +0100

    ✨ feat: habilitar express para usar rota de utilizadores

--- apps/TatameCore/src/routes/index.js
+++ apps/TatameCore/src/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import modalidadesRoutes from '../modalidades/modalidades.routes.js';
+import utilizadoresRoutes from '../utilizadores/utilizadores.routes.js';
 const router = express.Router();
 
 router.use('/modalidades', modalidadesRoutes);
+router.use('/utilizadores', utilizadoresRoutes);
 
 
 export default router;
