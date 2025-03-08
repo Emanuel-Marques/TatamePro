523c7a9849e168a676cd9eee1e11b268ca01d24e
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-03-08 14:55:10 +0100

    :sparkles: feat: model de alunos

--- apps/TatameCore/src/alunos/alunos.model.js
+++ apps/TatameCore/src/alunos/alunos.model.js
@@ -1,12 +1,12 @@
-import { connection } from "../database/config";
+import { connection } from "../database/config.js";
 
-export async function create(aluno) {
+async function create(aluno) {
   const {
     utilizadorId,
     modalidadeId,
     nome,
     numeroBI,
-    dataNascimnto,
+    dataNascimento,
     nomePai,
     nomeMae,
     endereco,
@@ -14,28 +14,95 @@ export async function create(aluno) {
     telefone,
     email,
     bolseiro,
-  } = aluno; 
-  
+  } = aluno;
+
   const query = `
     INSERT INTO alunos (utilizador_id, modalidade_id, nome, numero_bi, data_nascimento, nome_pai, nome_mae, endereco, genero,telefone, email, bolseiro, created_at) 
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())
     ON DUPLICATE KEY UPDATE created_at = NOW();
     `;
   try {
-    const [result] = await connection.query(query, [ utilizadorId,
-        modalidadeId,
-        nome,
-        numeroBI,
-        dataNascimnto,
-        nomePai,
-        nomeMae,
-        endereco,
-        genero,
-        telefone,
-        email,
-        bolseiro]);
+    const [result] = await connection.query(query, [
+      utilizadorId,
+      modalidadeId,
+      nome,
+      numeroBI,
+      dataNascimento,
+      nomePai,
+      nomeMae,
+      endereco,
+      genero,
+      telefone,
+      email,
+      bolseiro,
+    ]);
     return result;
   } catch (error) {
     console.error("Erro ao cadastrar o aluno :", error);
   }
 }
+
+async function getAll() {
+  const query = `
+    SELECT * FROM alunos;
+    `;
+  try {
+    const [result] = await connection.query(query);
+    return result;
+  } catch (error) {
+    console.error("Erro ao buscar alunos: ", error);
+  }
+}
+
+async function getById(alunoId) {
+  const query = `
+    SELECT * FROM alunos 
+    WHERE aluno_id = ?;
+    `;
+  try {
+    const [result] = await connection.query(query, [alunoId]);
+    return result;
+  } catch (error) {
+    console.error("Erro ao buscar aluno pelo id: ", error);
+  }
+}
+
+async function update(alunoId, utilizadorId, modalidadeId, nome, numeroBI, dataNascimento, nomePai, 
+  nomeMae, endereco, genero, telefone, email, bolseiro) {
+  const query = `
+    UPDATE alunos SET utilizador_id = ?, modalidade_id = ?, nome = ?, numero_bi = ?, 
+    data_nascimento = ?, nome_pai = ?, nome_mae = ?, endereco = ?, genero = ?, telefone = ?,
+    email = ?, bolseiro = ?  
+    WHERE aluno_id = ?;
+    `;
+  try {
+    const [result] = await connection.query(query, [
+      utilizadorId, modalidadeId, nome, numeroBI, dataNascimento, nomePai, 
+      nomeMae, endereco, genero, telefone, email, bolseiro, alunoId,
+    ]);
+    return result;
+  } catch (error) {
+    console.error("Erro ao actualizar dados do professor: ", error);
+  }
+}
+
+async function deleteAluno(alunoId) {
+  const query = `
+    DELETE FROM aluno 
+    WHERE aluno_id = ?;
+    `;
+  try {
+    const [result] = await connection.query(query, [alunoId]);
+    return result;
+  } catch (error) {
+    console.error("Erro ao apagar dados do aluno: ", error);
+  }
+}
+
+export default {
+  create,
+  getAll,
+  getById,
+  update,
+  deleteAluno,
+};
