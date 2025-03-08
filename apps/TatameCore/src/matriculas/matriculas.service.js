cc0bdde423a44e928c26a1af50452dde1913332b
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-03-08 15:37:38 +0100

    ✨ feat: service de matriculas, cadastro, actualização, consulta e anulação

---
+++ apps/TatameCore/src/matriculas/matriculas.service.js
@@ -0,0 +1,33 @@
+import matriculasService from './matriculas.model.js';
+async function create(matricula) {
+  const result = await matriculasService.create(matricula);
+  return result;
+}
+
+async function getAll() {
+    const result = await matriculasService.getAll();
+    return result;
+}
+
+async function getById(matriculaId) {
+    const result = await matriculasService.getById(matriculaId);
+    return result;
+}
+
+async function update(matriculaId, alunoId, planoId, dataInicio, estado) {
+    const result = await matriculasService.update(matriculaId, alunoId, planoId, dataInicio, estado);
+    return result;
+}
+
+async function deleteMatricula(matriculaId) {
+    const result = await matriculasService.deleteMatricula(matriculaId);
+    return result;
+}
+
+export default {
+  create,
+  getAll,
+  getById,
+  update,
+  deleteMatricula
+};
