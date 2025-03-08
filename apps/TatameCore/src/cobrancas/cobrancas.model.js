3904c5dc66e45fbbdc5b3e71c9c99f69feded794
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-03-09 00:31:31 +0100

    :sparkles: feat: criar cobranças no banco de dados

---
+++ apps/TatameCore/src/cobrancas/cobrancas.model.js
@@ -0,0 +1,17 @@
+import { connection } from '../database/config.js';
+async function create({ matriculaId, valor, dataVencimento, estado }){
+    const query = `
+        INSERT INTO cobrancas (matricula_id, valor, data_vencimento, estado)
+        VALUES (?, ?, ?, ?)
+    `;
+    try {
+        const [result] = await connection.execute(query, [matriculaId, valor, dataVencimento, estado]);
+        return result;
+    } catch (error) {
+        console.log('Erro ao gerar cobrança: ', error);
+    }
+}
+
+export default {
+    create
+}
