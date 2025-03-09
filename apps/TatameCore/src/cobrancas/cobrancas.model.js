e3a56034ea1b9cb7e81688675740114a8ac0e049
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-03-09 03:05:04 +0100

    ✨ feat: buscar cobrança pela data e matricula

--- apps/TatameCore/src/cobrancas/cobrancas.model.js
+++ apps/TatameCore/src/cobrancas/cobrancas.model.js
@@ -1,17 +1,32 @@
 import { connection } from '../database/config.js';
-async function create( matriculaId, valor, dataVencimento, estado ){
+async function create( matriculaId, valor, dataVencimento ){
     const query = `
         INSERT INTO cobrancas (matricula_id, valor, data_vencimento, estado)
-        VALUES (?, ?, ?, ?)
+        VALUES (?, ?, ?, 'Pendente')
+        ON DUPLICATE KEY UPDATE estado = 'Pendente', valor = VALUES(valor)
     `;
     try {
-        const [result] = await connection.execute(query, [matriculaId, valor, dataVencimento, estado]);
+        const [result] = await connection.execute(query, [matriculaId, valor, dataVencimento]);
         return result;
     } catch (error) {
         console.log('Erro ao gerar cobrança: ', error);
     }
 }
 
+async function getByMatriculaEData( matriculaId, dataVencimento ){
+    const query = `
+        SELECT * FROM cobrancas 
+        WHERE matricula_id = ? AND data_vencimento = ?
+    `;
+    try {
+        const [result] = await connection.execute(query, [matriculaId, dataVencimento]);
+        return result;
+    } catch (error) {
+        console.log('Erro ao buscar cobrança pela matricula e data de vencimento: ', error);
+    }
+}
+
 export default {
-    create
+    create,
+    getByMatriculaEData
 }
--- apps/TatameCore/src/cobrancas/cobrancas.service.js
+++ apps/TatameCore/src/cobrancas/cobrancas.service.js
@@ -1,10 +1,16 @@
 import cobrancasModel from "./cobrancas.model.js";
 
-async function create(matriculaId, valor, dataVencimento, estado){
-    const result = await cobrancasModel.create(matriculaId, valor, dataVencimento, estado);
+async function create(matriculaId, valor, dataVencimento){
+    const result = await cobrancasModel.create(matriculaId, valor, dataVencimento);
     return result;
 }
 
+async function getByMatriculaEData(matriculaId, dataVencimento){
+    const result = await cobrancasModel.getByMatriculaEData(matriculaId, dataVencimento);
+    return result; 
+}
+
 export default {
-    create
+    create,
+    getByMatriculaEData
 }
